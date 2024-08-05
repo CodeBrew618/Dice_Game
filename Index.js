364d6d30a78df09b93dcea1163c8b307715c518c
Charles L <charlieliu@Charlies-MacBook-Pro.local>
2024-08-05 16:54:44 -0400

    add another Starting Page.

--- Index.js
+++ Index.js
@@ -1,28 +1,33 @@
- var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+function roll() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
- var randomDiceImage = "dice" + randomNumber1 + ".png";
+    var randomDiceImage = "dice" + randomNumber1 + ".png";
 
- var randomImageSource = "images/" + randomDiceImage;
+    var randomImageSource = "images/" + randomDiceImage;
 
- var image1 = document.querySelectorAll("img")[0]
+    var image1 = document.querySelectorAll("img")[0]
 
- image1.setAttribute("src", randomImageSource);
-
-
+    image1.setAttribute("src", randomImageSource);
 
 
- var randomNumber2 = Math.floor(Math.random()*6) + 1;
- var randomDiceImage = "dice" + randomNumber2 + ".png";
- var randomImageSource2 = "images/" + randomDiceImage;
 
- var image2 = document.querySelectorAll("img")[1];
- image2.setAttribute("src", randomImageSource2);
 
- if (randomNumber1 > randomNumber2) {
+    var randomNumber2 = Math.floor(Math.random()*6) + 1;
+    var randomDiceImage = "dice" + randomNumber2 + ".png";
+    var randomImageSource2 = "images/" + randomDiceImage;
 
-    document.querySelector("h1").innerHTML = "🤩 Charles wins!"
- }else if (randomNumber1 == randomNumber2){
-    document.querySelector("h1").innerHTML = "🤯 Draw~ 🫠"
- } else{
-    document.querySelector("h1").innerHTML = "🥳 Aaron wins!"
+    var image2 = document.querySelectorAll("img")[1];
+    image2.setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+
+        document.querySelector("h1").innerHTML = "🤩 Charles wins!"
+    }else if (randomNumber1 == randomNumber2){
+        document.querySelector("h1").innerHTML = "🤯 Draw~ 🫠"
+    } else{
+        document.querySelector("h1").innerHTML = "🥳 Aaron wins!"
+    }
+
  }
+ 
+ roll();
